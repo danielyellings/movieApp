2a9059c55b4a021ed9daa974f5477e8db2d51e7e
Daniyar <yerkinov.daniyar@yandex.kz>
2024-03-23 22:22:17 +0500

    deleted .env and node_modules

--- models/user.js
+++ models/user.js
@@ -1,3 +1,23 @@
 //models
 
 const postgres = require('postgres')
+//Definition of user schema
+const userSchema = new postgres.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+const User = postgres.model('User', userSchema)
+module.exports = User
--- static.js
+++ static.js
@@ -20,45 +20,12 @@ const tmdbApi = axios.create({
 });
 
 
-
-
 app.get('/popular-movies', async (req, res) => {
   try {
     const response = await tmdbApi.get('/movie/popular');
     const movies = response.data.results;
 
     return res.json({ movies });
-
-    // TODO: RETURN MOVIES WITHOUT SAVING TO DATABASE
-    // const insertMovieQueries = movies.map(movie => {
-    //   const title = movie.title.replace(/'/g, "''");
-    //   const overview = movie.overview ? movie.overview.replace(/'/g, "''") : '';
-    //   const releaseDate = movie.release_date ? `'${movie.release_date}'` : 'NULL';
-    //   const posterPath = movie.poster_path ? `'${movie.poster_path}'` : 'NULL';
-
-    //   return `INSERT INTO movies (title, overview, release_date, poster_path, tmdb_id)
-    //           VALUES ('${title}', '${overview}', ${releaseDate}, ${posterPath}, ${movie.id})
-    //           ON CONFLICT (tmdb_id) DO NOTHING;`;
-    // });
-
-    // const psqlCommand = `psql -d ${process.env.DB_NAME} -U ${process.env.DB_USER} -c "${insertMovieQueries.join(' ')}"`;
-
-    // exec(psqlCommand, (error, stdout, stderr) => {
-    //   if (error) {
-    //     console.error(`Error executing psql: ${error}`);
-    //     res.status(500).json({ error: 'An error occurred while fetching and inserting movies' });
-    //     return;
-    //   }
-
-      // if (stderr) {
-      //   console.error(`stderr: ${stderr}`);
-      //   res.status(500).json({ error: 'An error occurred while fetching and inserting movies' });
-      //   return;
-      // }
-
-    //   console.log(`stdout: ${stdout}`);
-    //   res.json({ message: 'Movies data inserted successfully' });
-    // });
   } catch (error) {
     console.error('An error occurred while fetching movies:', error);
     res.status(500).json({ error: 'An error occurred while fetching movies' });
