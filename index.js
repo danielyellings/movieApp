fb6c05e8bf8e7248befa22665da649b55500639c
Daniyar <yerkinov.daniyar@yandex.kz>
2024-03-26 21:45:29 +0500

    added registration logic to app

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const db = require('./db.js');
 const path = require('path');
 const axios = require('axios');
 const dotenv = require('dotenv');
 const { Pool } = require('pg')
 const bodyParser = require('body-parser')
 dotenv.config();
+const { pgTable, serial, text, varchar } = require("drizzle-orm/pg-core");
+const { drizzle } = require("drizzle-orm/node-postgres");
+
+// const pool = new Pool({
+//   connectionString: "postgres://user:password@host:port/db",
+// });
 
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const app = express();
@@ -22,6 +29,16 @@ var pool = new Pool({
   connectionTimeoutMillis: 1000,
   maxUses: 7500,
 })
+
+// app.get('/', async (req, res) => {
+//   try {
+//     const result = await db.query('SELECT * FROM users');
+//     res.json(result.rows);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 //sendFile go here
 app.get('/', (req, res) => {
@@ -56,6 +73,31 @@ app.get('/popular-movies', async (req, res) => {
     res.status(500).json({ error: 'An error occurred while fetching movies' });
   }
 });
+
+//registration logic
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  pool.query('SELECT FROM users WHERE username = $1 OR email = $2', [username, password], (err, results) => {
+    if (err) {
+      console.error('Error during executing the request', err)
+      res.status(500).json({ error: "Error during new user's registration"})
+    } else {
+      if (results.rows.length > 0) {
+        res.status(400).json({ error: 'User with these username or email already registered'})
+      } else {
+        pool.query('INSERT INTO users (username, password, email) VALUES ($1, $2, $3)', [username, password, email], (err, results) => {
+          if (err) {
+            console.error('Error during registratin of new user', err)
+            res.status(500).json({ error: 'Error during registration of new user'})
+          } else {
+            res.send('Registration is successful!')
+          }
+        })
+      }
+    }
+  })
+})
 
 //post request to log into main page
 app.post('/login', (req, res) => {
